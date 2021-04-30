611110cdc46674d27086401ca696d31a69000ad3
portega017 <46809918+portega017@users.noreply.github.com>
2021-04-30 11:34:26 +0200

    Create SubirGit.sh

---
+++ SubirGit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Nombre
+echo "¿Cual es tu nombre?"
+read NOMBRE
+
+# Workspace
+cd ~/Examenes-ASI/$NOMBRE
+
+# Commit message
+echo "¿Que has hecho hoy?"
+read COMMIT
+
+# Git
+git pull
+git add -A
+git commit -m "$COMMIT"
+git push
