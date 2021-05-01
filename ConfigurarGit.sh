5867028b25362344b799979874b16c4020ce9d1f
Gaizka Martin <gmmendiola@gmail.com>
2021-05-01 18:22:19 +0200

    Actualizado ConfigurarGit.sh

--- ConfigurarGit.sh
+++ ConfigurarGit.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
-# Nombre
-echo "¿Cual es tu nombre?"
+# Salto de linea dependiendo de tu versión de echo
+if [ "`echo -n`" = "-n" ]; then
+  n=""
+  c="\c"
+else
+  n="-n"
+  c=""
+fi
+
+# Nombre y correo
+echo $n "¿Que nombre quieres ponerle a tu carpeta de GitHub?"
 read NOMBRE
+
+echo $n "¿Cual es tu nombre completo?"
+read NOMBRECOMPLETO
+
+echo $n "¿Cual es tu correo de GitHub?"
+read CORREO
 
 # Git initial configuration
 cd ~
+sudo apt install git -y
 git config --global credential.helper store
+git config --global user.name "$NOMBRECOMPLETO"
+git config --global user.email "$CORREO"
 git clone https://github.com/portega017/Examenes-STA.git
 
 # Workspace
