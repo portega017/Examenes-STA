4c6db695ca6b65035333b881170c7407972b5474
Gaizka Martin <gmmendiola@gmail.com>
2021-05-13 13:05:30 +0200

    Sin acabar

--- Gaizka/Mayo13/Mayo13/src/bl/LogicaNegocio.java
+++ Gaizka/Mayo13/Mayo13/src/bl/LogicaNegocio.java
@@ -8,7 +8,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import dl.Contacto;
-import pl.modelo.ContactBean;
 
 @Stateless
 @LocalBean
@@ -17,16 +16,27 @@ public class LogicaNegocio {
 	@PersistenceContext
 	private EntityManager em;
 	
-	public void altaContacto() {
-		
+	public void altaContacto(Contacto c) {
+		if(c != null) {
+			if(!em.createNamedQuery("Contacto.findByName").setParameter("nombre", c.getNombre()).getResultList().isEmpty()) {
+				em.persist(c);
+			}
+		}
 	}
 	
-	public void bajaContacto() {
-		
+	public void bajaContacto(int idProducto) {
+		Contacto c = em.find(Contacto.class, idProducto);
+		if(c != null) {
+			em.remove(c);
+		}
 	}
 	
-	public Contacto buscarContacto() {
-		
+	public Contacto buscarContacto(String nombre) {
+		Contacto c = em.createNamedQuery("Contacto.findByNombre",Contacto.class).getSingleResult();
+		if(c == null) {
+			c = new Contacto();
+		}
+		return c;
 	}
 	
 	public List<Contacto> getContactos(){
--- Gaizka/Mayo13/Mayo13/src/pl/modelo/AgendaBean.java
+++ Gaizka/Mayo13/Mayo13/src/pl/modelo/AgendaBean.java
@@ -1,13 +1,51 @@
 package pl.modelo;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 import bl.LogicaNegocio;
+import dl.Contacto;
 
-public class AgendaBean {
+@Named
+@ViewScoped
+public class AgendaBean implements Serializable {
 
 	@EJB
 	private LogicaNegocio ln = new LogicaNegocio();
+	private List<Contacto> lc = new ArrayList<Contacto>();
+	private static final long serialVersionUID = 1L;
 	
+	public void altaContacto(Contacto c) {
+		ln.altaContacto(c);
+	}
 	
+	public void bajaContacto(Contacto c) {
+		ln.bajaContacto(c.getIdCONTACTOS());
+	}
+
+	public Contacto buscarContacto() {
+		Contacto c = ln.buscarContacto(nombreBuscar);
+		
+		return c;
+	}
+	
+	public List<Contacto> getContactos() {
+		if(lc == null) {
+			lc = ln.getContactos();
+		}
+		return lc;
+	}
+
+	public String getNombreBuscar() {
+		return nombreBuscar;
+	}
+
+	public void setNombreBuscar(String nombreBuscar) {
+		this.nombreBuscar = nombreBuscar;
+	}
 }
